2d41fa6a13b9ce7ecac161b2464aff3ff0e8674c
Uier <ap9940506@gmail.com>
2020-06-29 03:14:45 +0800

    improve some design

--- CMapUnit.h
+++ CMapUnit.h
@@ -1,5 +1,6 @@
 #include "util.h"
-#include <vector>
+#include <set>
+#include <assert.h>
 
 const int MAX_LEVEL = 5;
 const int MAX_Player = 4;
@@ -9,34 +10,34 @@ class MapUnit {
 public:
 	/*
 		TODO:
-			- Destructor ?
-			- Hiding data to private
-			- func: Release Units
+			- Buy Method (change owner)
 	*/
-	/*
-		Constructor
-	*/
+
 	MapUnit(
+		const char &type
 		const int &id,
 		const std::string &name,
-		const int &price
-	) : ID(id),
-		PRICE(price),
-		NAME(name) {}
+		const int &price = -1
+	) : TYPE(type),
+		ID(id),
+		NAME(name),
+		PRICE(price) {}
+
 	/*
-		Properties
+		TODO: 
+			- move some data to protected
 	*/
+	const char TYPE;
 	const int ID, PRICE;
 	const std::string NAME;
 	int owner = NULL;
-	/*
-		Methods
-	*/
+
+	bool isJail() { return (TYPE == 'J'); }
+	bool isOwner(const int &player) { return (owner == player); }
+	bool buyable() { return (!isJail() && owner == NULL); }
 	virtual int travelFine();
-	virtual bool isJail() { return false; }
-	virtual bool buyable() { return (owner == NULL); }
-	virtual void setOwner(int user_id) { owner = user_id; }
-	void releaseUnit();
+	virtual bool upgradable() { return false; }
+	virtual void release() { owner = NULL; }
 
 private:
 
@@ -46,38 +47,34 @@ private:
 class UpgradableUnit : public MapUnit {
 	
 public:
-	/*
-		TODO:
-			- Destructor ?
-			- Hiding data to private
-	*/
-	/*
-		Constructor
-	*/
 	UpgradableUnit(
 		const int &id, 
 		const std::string &name,
 		const int &price,
 		const int &upgrade_price,
 		const int *fine_of_level
-	) : MapUnit(id, name, price), 
+	) : MapUnit('U', id, name, price), 
 		UPGRADE_PRICE(upgrade_price), 
 		FINE_OF_LEVEL(fine_of_level) {}
-	/*
-		Properties
-	*/
-	const int UPGRADE_PRICE;
-	const int FINE_OF_LEVEL[MAX_LEVEL];
-	int level = 0;
-	/*
-		Methods
-	*/
-	virtual int travelFine() {
+
+	~UpgradableUnit() { delete[] FINE_OF_LEVEL; }
+
+	const int & getUpgradePrice() { return UPGRADE_PRICE; }
+	virtual const int & travelFine() const {
 		assert(0 <= level < MAX_LEVEL);
 		return FINE_OF_LEVEL[level];
 	}
+	virtual bool upgradable() const { return (level+1 < MAX_LEVEL); }
+	void upgrade() { level += 1; }
+	virtual void release() {
+		owner = NULL;
+		level = 0;
+	}
 
 private:
+	const int UPGRADE_PRICE;
+	const int *FINE_OF_LEVEL;
+	int level = 0;
 
 };
 
@@ -85,39 +82,23 @@ private:
 class CollectableUnit : public MapUnit {
 	
 public:
-	/*
-		TODO:
-			- Destructor ?
-			- Hiding data to private
-	*/
-	/*
-		Constructor
-	*/
 	CollectableUnit(
 		const int &id, 
 		const std::string &name,
 		const int &price,
 		const int &unit_fine,
-	) : MapUnit(id, name, price),
+	) : MapUnit('C', id, name, price),
 		UNIT_FINE(unit_fine) {}
-	/*
-		Properties
-	*/
-	const int UNIT_FINE;
+
 	static int number_of_units_of_owner[MAX_Player];
-	/*
-		Methods
-	*/
+
 	virtual int travelFine() {
 		assert(0 <= owner < MAX_Player);
 		return number_of_units_of_owner[owner] * UNIT_FINE;
 	}
-	virtual void setOwner(int user_id) {
-		owner = user_id;
-		number_of_units_of_owner[owner] += 1;
-	}
 
 private:
+	const int UNIT_FINE;
 
 };
 
@@ -125,62 +106,36 @@ private:
 class RandomCostUnit : public MapUnit {
 	
 public:
-	/*
-		TODO:
-			- Destructor ?
-			- Hiding data to private
-	*/
-	/*
-		Constructor
-	*/
 	RandomCostUnit(
 		const int &id, 
 		const std::string &name,
 		const int &price,
 		const int &unit_fine,
-	) : MapUnit(id, name, price),
+	) : MapUnit('R', id, name, price),
 		UNIT_FINE(unit_fine) {}
-	/*
-		Properties
-	*/
-	const int UNIT_FINE;
-	/*
-		Methods
-	*/
-	virtual int travelFine() {
-		return randint(1, 6) * UNIT_FINE;
-	}
+
+	virtual int travelFine() { return randint(1, 6) * UNIT_FINE; }
 
 private:
+	const int UNIT_FINE;
 
 };
 
 
 class JailUnit : public MapUnit {
+
 public:
-	/*
-		TODO:
-			- Destructor ?
-			- Hiding data to private
-	*/
-	/*
-		Constructor
-	*/
 	JailUnit(
 		const int &id, 
 		const std::string &name,
 		const int &price,
-	) : MapUnit(id, name, price) {}
-	/*
-		Properties
-	*/
-	vector<int> player_in_jail;
-	/*
-		Methods
-	*/
-	virtual bool isJail() { return true; }
+	) : MapUnit('J', id, name, price) {}
+
+	void addPlayer(int player) { player_in_jail.insert(player); }
+	void removePlayer(int player) { player_in_jail.erase(player); }
 
 private:
+	set<int> player_in_jail;
 
 };
 
